4462b1f55643474f6053798f52b19d603b58fd19
amureki <fly.amureki@gmail.com>
2014-05-23 21:50:52 +0600

    you can vote now on index page

--- project/staticfiles/js/main.js
+++ project/staticfiles/js/main.js
@@ -5,19 +5,20 @@ HideWarningAlert();
 function BuildRateChange() {
     $('button#down, button#up').click(function (data) {
         var action = $(this).data('value');
-        var build_id = window.location.pathname.replace(/[^\d.]/g, '');
+        var build_id = $(this).parent().attr('id');
         var csrftoken = $(this).parent().find("[name='csrfmiddlewaretoken']");
-//        alert(window.location.pathname);
+        var alert_box = $('div#' + build_id + '.alert')
+        var rating_box = $('span#' + build_id + '.rating')
         $.ajax({
             url: '/vote/',
             type: "POST",
             data: "id=" + build_id + "&action=" + action + "&csrfmiddlewaretoken=" + csrftoken.val(),
             success: function (data) {
                 if (data.status.toLowerCase() === "success") {
-                    $('span#rating').html(data.data.rating);
+                    rating_box.html(data.data.rating);
                 }
                 else if (data.status.toLowerCase() === "fail") {
-                    $(".alert").show()
+                    alert_box.show()
                 }
             },
             error: function (data) {
